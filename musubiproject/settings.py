0c856d99ed789dc39f331a6b908a95df16922347
leigh <honeyleighdeanmendozamontes@gmail.com>
2025-11-21 14:51:34 +0800

    pls

--- musubiproject/settings.py
+++ musubiproject/settings.py
@@ -1,6 +1,6 @@
-
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,10 +67,10 @@ WSGI_APPLICATION = 'musubiproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
